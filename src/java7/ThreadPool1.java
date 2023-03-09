237b7438adc5cc422cb2408389d4c6ba9efc0279
TTN-Mohammad_farhan <mohammad.farhan@tothenew.com>
2023-03-09 12:37:13 +0530

    execute wala code added

--- src/java7/ThreadPool1.java
+++ src/java7/ThreadPool1.java
@@ -1,14 +1,18 @@
 package java7;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPool1 {
 	public static void main(String[ ] args)
 	{
 		Runnable r=()->System.out.println(Thread.currentThread().getName()+"--- is running");
-		
+		ExecutorService service=Executors.newFixedThreadPool(5);
 		for(int i=0;i<50;i++)
 		{
-			Thread t=new Thread(r);
-			t.start();
+//			Thread t=new Thread(r);
+//			t.start();
+			service.execute(r);
 		}
 	}
 
