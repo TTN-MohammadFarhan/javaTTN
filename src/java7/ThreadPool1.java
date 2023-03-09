4d76babc03362547105d4f825b2c8ebf0c476d08
TTN-Mohammad_farhan <mohammad.farhan@tothenew.com>
2023-03-09 12:38:58 +0530

    cachedTheadPool syntexed added

--- src/java7/ThreadPool1.java
+++ src/java7/ThreadPool1.java
@@ -7,7 +7,8 @@ public class ThreadPool1 {
 	public static void main(String[ ] args)
 	{
 		Runnable r=()->System.out.println(Thread.currentThread().getName()+"--- is running");
-		ExecutorService service=Executors.newFixedThreadPool(5);
+		//ExecutorService service=Executors.newFixedThreadPool(5);
+		ExecutorService service=Executors.newCachedThreadPool();
 		for(int i=0;i<50;i++)
 		{
 //			Thread t=new Thread(r);
